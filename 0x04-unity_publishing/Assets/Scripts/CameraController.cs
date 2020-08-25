6e48955d6c278a45a087ab1ad52e7cdbb55e639c
CutiePizza <ines.chokri97@gmail.com>
2020-08-24 22:28:08 -0300

    unity project maze

---
+++ 0x04-unity_publishing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+        // Without Capital T refers to our current object
+        transform.position = new Vector3(player.position.x, 26, player.position.z)  + offset;
+    }
+}
+ 
---
+++ 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private int score;
+    public float speed;
+    public Rigidbody rb;
+    public int health;
+    public Text scoreText;
+    public Text healthText;
+
+    public Text youWinLose;
+
+    public Image bg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       score = 0;
+       health = 5;
+    }
+
+    void FixedUpdate()
+    {
+    
+     if (Input.GetKey("d") || Input.GetKey("right"))
+     {
+        rb.AddForce(speed * Time.deltaTime, 0, 0);
+     }
+
+     if (Input.GetKey("a") || Input.GetKey("left"))
+     {
+        rb.AddForce(-speed * Time.deltaTime, 0, 0);
+     }
+
+     if (Input.GetKey("s") || Input.GetKey("down"))
+     {
+        rb.AddForce(0, 0, -speed * Time.deltaTime);
+     }
+
+     if (Input.GetKey("w") || Input.GetKey("up"))
+     {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+     }     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+           bg.color = Color.red;
+           youWinLose.text = "Game Over!".ToString();
+           youWinLose.color = Color.white;
+           bg.gameObject.SetActive(true);
+           StartCoroutine(LoadScene(3));
+        }
+    }
+    IEnumerator LoadScene(float seconds)
+    {
+       yield return new WaitForSeconds(seconds);
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex,LoadSceneMode.Single);
+    }
+   void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void SetHealthText()
+    {
+       healthText.text = "Health: " + health.ToString();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.tag == "Pickup")
+       {
+          score++;
+          Destroy(other.gameObject);
+          SetScoreText();
+         // Debug.Log($"Score: {score}");
+       }
+       if (other.gameObject.tag == "Trap")
+       {
+          health--;
+          SetHealthText();
+          // Debug.Log($"Health {health}");
+       }
+
+       if (other.gameObject.tag == "Goal")
+       { 
+          bg.color = Color.green;
+          youWinLose.text = "You Win!".ToString();
+          youWinLose.color = Color.black;
+          bg.gameObject.SetActive(true);
+          StartCoroutine(LoadScene(3));
+       }
+    }
+
+}
