c99c2072acf626605cdf45d9ffba36c7b1e4da48
CutiePizza <ines.chokri97@gmail.com>
2020-09-09 21:50:20 -0300

    Added skybox and lightning

--- 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinTrigger : MonoBehaviour
 {
+    Timer timer;
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,15 @@ public class WinTrigger : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            player = GameObject.Find("Player");
+            timer = player.GetComponent<Timer>();
+            timer.enabled = false;
+            timer.timerText.color = Color.green;
+            timer.timerText.fontSize = 80;
+
     }
 }
