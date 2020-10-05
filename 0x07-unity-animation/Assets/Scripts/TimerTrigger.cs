c14b89f5662a48c2b4f359ef73085b817e80229a
CutiePizza <ines.chokri97@gmail.com>
2020-10-05 17:29:19 -0300

    Creation of animation and animators for camera

---
+++ 0x07-unity-animation/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public bool isTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            other.GetComponent<Timer>().enabled = true;
+            isTrigger = true;
+    }
+}
---
+++ 0x07-unity-animation/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    Timer timer;
+    GameObject player;
+    public Canvas WinMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            player = GameObject.Find("Player");
+            timer = player.GetComponent<Timer>();
+            timer.enabled = false;
+            timer.timerText.color = Color.green;
+            timer.timerText.fontSize = 80;
+            WinMenu.gameObject.SetActive(true);
+    }
+}
