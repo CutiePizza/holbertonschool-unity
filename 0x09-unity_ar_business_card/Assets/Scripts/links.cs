f1daa10b8094e1c90bbd1e9e4c0580189c93816f
CutiePizza <ines.chokri97@gmail.com>
2020-11-05 14:41:28 -0300

    Card

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/links.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class links : MonoBehaviour
+{
+    public void ToEmail()
+    {
+        Application.OpenURL("mailto:ines.chokri97@gmail.com");
+    }
+
+    public void Linkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/inès-chokri-b247b7175/");
+    }
+
+    public void GitHub()
+    {
+        Application.OpenURL("https://github.com/CutiePizza");
+    }
+
+    public void Twitter()
+    {
+        Application.OpenURL("https://twitter.com/chokri_ines");
+    }
+}
